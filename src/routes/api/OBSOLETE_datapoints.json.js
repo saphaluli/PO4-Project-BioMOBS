3ddd857c3c88dd1fc5ad559d17ef626bdb2749c6
“Driesheylen1” <heylendries4@gmail.com>
2022-11-15 14:26:03 +0100

    arangoDBcollections_renamed

--- src/routes/api/OBSOLETE_datapoints.json.js
+++ src/routes/api/OBSOLETE_datapoints.json.js
@@ -3,7 +3,7 @@ import { aql } from "arangojs"
 export let data = {}
 
 export async function get() {
-    const query_datapoints = aql`FOR x IN T2D_samples RETURN x`;
+    const query_datapoints = aql`FOR x IN bioMOBS_test_samples RETURN x`;
     const cursor_datapoints = await db.query(query_datapoints, { batchSize: 10 });
     for await (const batch of cursor_datapoints.batches) {
         // Process all values in a batch in parallel
--- src/routes/api/OBSOLETE_feature_datapoints.json.js
+++ src/routes/api/OBSOLETE_feature_datapoints.json.js
@@ -3,7 +3,7 @@ import { aql } from "arangojs"
 export let data = {}
 
 export async function get() {
-    const query_datapoints = aql`FOR x IN Use_case_UKB_Features RETURN x`;
+    const query_datapoints = aql`FOR x IN bioMOBS_testdata_features RETURN x`;
     const cursor_datapoints = await db.query(query_datapoints, { batchSize: 10 });
     for await (const batch of cursor_datapoints.batches) {
         // Process all values in a batch in parallel
